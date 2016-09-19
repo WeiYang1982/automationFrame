89e6d9312c2ead943ef56140535a4009a2f86461
A <jianxing12345@sina.com>
2016-09-19 16:48:22 +0800

    修改测试框架结构： 1.使用LoadableComponent 2.修改startDriver的启动方式 3.修改LogWriter的参数读取方式 4.使用reportng

--- src/main/java/pageElement/pageHome.java
+++ src/main/java/pageElement/pageHome.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
 /**
  * Created by A on 2016/2/4.
  */
-public class PageHome {
+public class PageHome extends LoadableComponent<PageHome>{
     WebDriver driver;
 
     public PageHome( WebDriver driver ){
@@ -18,4 +19,12 @@ public class PageHome {
 
     @FindBy( linkText = "图片管理" )
     public WebElement picConfig;
+
+    protected void load() {
+
+    }
+
+    protected void isLoaded() throws Error {
+
+    }
 }
